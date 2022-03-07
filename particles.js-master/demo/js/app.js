7f52a1ebebe7014e99641015278fc1d00340d8e9
Ismail <73080610+chuma6@users.noreply.github.com>
2022-03-07 16:05:48 +0100

    Update app.js

--- particles.js-master/demo/js/app.js
+++ particles.js-master/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 600,
+        "value": 200,
         "density": {
           "enable": true,
           "value_area": 800
